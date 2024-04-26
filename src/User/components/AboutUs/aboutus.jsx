d1959a77802597dc49ec01469ce7a1c603d917bd
Mehrab-developer <110405427+Mehrab-developer@users.noreply.github.com>
2024-04-26 15:06:19 +0500

    image issue fix

--- src/User/components/AboutUs/aboutus.jsx
+++ src/User/components/AboutUs/aboutus.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useForm } from "react-hook-form";
 import "./aboutus.css";
 import { Bed, Champagne, ForkKnife } from "@phosphor-icons/react";
 const AboutUs = () => {
